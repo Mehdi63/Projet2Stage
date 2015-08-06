0e0f95a965e9fe6692564f47f19c611873b7d486
elhaddad94 <elhaddad94@hotmail.fr>
2015-08-06 17:23:31 +0200

    commit post_final
    Ajout des balise :
    - id,
    - entity id,
    - surname,
    - firstname,
    - shs_discipline ,
    - shs_sub_discipline.
    Factorisation du code.

--- xlsxtoxml/src/projetstage/Algo2.java
+++ xlsxtoxml/src/projetstage/Algo2.java
@@ -7,8 +7,6 @@ public class Algo2 {
    public static final String ICCF = "Institut de Chimie de Clermont-Ferrand ICCF, UMR 6296";
    public static final String LAMP = "Laboratoire de Météorologie Physique LaMP, UMR 6016";
    public static final String LM = "Laboratoire de Mathématiques LM, UMR 6620";
-   @SuppressWarnings("unused")
-   private static String TYPE;
 
    private Algo2() {
    }
@@ -16,7 +14,7 @@ public class Algo2 {
    static void remplir(String[][] resRegroupe)
          throws IOException {
       for (int i = Config.O; i < Config.XII + Config.COMPETENCES.length; i++) {
-         String[] fiche = new String[Config.XIX];
+         String[] fiche = new String[Config.XXIII];
          fillAnArrays1(fiche, i, resRegroupe);
          fillAnArrays2(fiche, i, resRegroupe);
          fillAnArrays3(fiche, i, resRegroupe);
@@ -29,12 +27,7 @@ public class Algo2 {
    static void toRefactor(int i, int cpt, String[] fiche, String[][] resRegroupe) {
       
       if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-         fiche[Config.IX] += " -"
-               + Config.EQUIPEMENTS[cpt - Config.MCCCLIII];
-         if (Test.isDifferentToSpace(resRegroupe, i, cpt + Config.I)) {
-            fiche[Config.IX] += "(" + resRegroupe[i][cpt + Config.I]
-                  + ") ";
-         }
+         fiche[Config.IX] += Config.EQUIPEMENTS[cpt - Config.MCCCLIII] + ", ";
       }
    }
 
@@ -67,54 +60,50 @@ public class Algo2 {
       fiche[Config.III]=Config.EMPTY;
       for (int cpt = Config.CCLXXXIII; cpt < Config.CCCXXXVIII; cpt++) {
          if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-            fiche[Config.III] += " -" +Config.DOMAINES[cpt - Config.CCLXXXIII]
-                  ;
+            fiche[Config.III] += Config.DOMAINES[cpt - Config.CCLXXXIII] + " / ";
             techneo = Config.I;
          }
       }
       if (Test.isDifferentToSpace(resRegroupe, i, Config.CCCX)) {
-         fiche[Config.III] += " -" +  resRegroupe[i][Config.CCCX] ;
+         fiche[Config.III] += resRegroupe[i][Config.CCCX] + " / ";
          techneo = Config.I;
       }
       if (Test.isDifferentToSpace(resRegroupe, i, Config.CCCXXXVIII)) {
-         fiche[Config.III] += " -" +  resRegroupe[i][Config.CCCXXXVIII] ;
+         fiche[Config.III] += resRegroupe[i][Config.CCCXXXVIII] + " / ";
          techneo = Config.I;
       }
       for (int cpt = Config.DCIII; cpt < Config.DCXXIX; cpt++) {
          if (resRegroupe[i][cpt].equals(Config.OUI)) {
-            fiche[Config.III] += " -" +  Config.DISCIPLINES[cpt - Config.DCIII]
-                  ;
+            fiche[Config.III] += Config.DISCIPLINES[cpt - Config.DCIII] + " / ";
          }
       }
       if (Test.isDifferentToSpace(resRegroupe, i, Config.DCXXIX)) {
-         fiche[Config.III] += " -" +  resRegroupe[i][Config.DCXXIX] ;
+         fiche[Config.III] += resRegroupe[i][Config.DCXXIX] + " / ";
       }
       if (techneo != Config.O) {
-         TYPE = Config.TECHNOLOGIQUE;
+         fiche[Config.XXII] = Config.TECHNOLOGIQUE;
       } else {
-         TYPE = Config.SHS;
+         fiche[Config.XXII] = Config.SHS;
       }
    }
 
    static void fillAnArrays3(String[] fiche, int i, String[][] resRegroupe) {
-       
+      
       fiche[Config.IV] = Config.EMPTY;
       for (int cpt = Config.CCCXXXIX; cpt < Config.DCII; cpt++) {
          if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-            fiche[Config.IV] += " -" +  Config.SOUS_DOMAINES[cpt - Config.CCCXXXIX]
-                  ;
+            fiche[Config.IV] += Config.SOUS_DOMAINES[cpt - Config.CCCXXXIX] + ", ";
          }
       }
       for (int cpt = Config.DCXXX; cpt < Config.CMXVIII; cpt++) {
          if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-            fiche[Config.IV] += " -" +  Config.SOUS_DISCIPLINES[cpt - Config.DCXXX]
-                  ;
+            fiche[Config.IV] += Config.SOUS_DISCIPLINES[cpt - Config.DCXXX] + ", ";
          }
       }
 
       for (Integer col : Config.TO_CLEAN) {
          if (Test.isDifferentToSpace(resRegroupe, i, col)) {
-            fiche[Config.IV] += " -" +  resRegroupe[i][col] ;
+            fiche[Config.IV] += resRegroupe[i][col] + ", ";
          }
       }
    }
@@ -124,22 +113,22 @@ public class Algo2 {
     fiche[Config.VI]=Config.EMPTY; 
       for (int cpt = Config.CMXVIII; cpt < Config.CMLV; cpt++) {
          if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-            fiche[Config.VI] += " -" +  Config.SECTEURS[cpt - Config.CMXVIII] ;
+            fiche[Config.VI] += Config.SECTEURS[cpt - Config.CMXVIII] + " / ";
          }
       }
       if (Test.isDifferentToSpace(resRegroupe, i, Config.CMLIV)) {
-         fiche[Config.VI] += " -" +  resRegroupe[i][Config.CMLIV] ;
+         fiche[Config.VI] += resRegroupe[i][Config.CMLIV] + " / ";
       }
 
       fiche[Config.VII] = Config.EMPTY;
       for (int cpt = Config.CMLV; cpt < Config.MCCCVI; cpt++) {
          if (Test.isEqualToYes(resRegroupe, i, cpt)) {
-            fiche[Config.VII] += " -" + Config.SOUS_SECTEURS[cpt - Config.CMLV];
+            fiche[Config.VII] += Config.SOUS_SECTEURS[cpt - Config.CMLV] + ", ";
          }
       }
       for (Integer col : Config.TO_CLEAN1) {
          if (Test.isDifferentToSpace(resRegroupe, i, col)) {
-            fiche[Config.VII] += " -" +  resRegroupe[i][col];
+            fiche[Config.VII] += resRegroupe[i][col] + ", ";
          }
       }
    }
@@ -149,27 +138,30 @@ public class Algo2 {
     fiche[Config.VIII]=Config.EMPTY;
     fiche[Config.IX]=Config.EMPTY;
     if (Test.isDifferentToSpace(resRegroupe, i, Config.MCCCVII)) {
-         fiche[Config.VIII] += " -" + resRegroupe[i][Config.MCCCVII];
+         fiche[Config.VIII] += resRegroupe[i][Config.MCCCVII] + ", ";
       }
       if (Test.isDifferentToSpace(resRegroupe, i, Config.MCCCVIII)) {
-         fiche[Config.VIII] += " -" + resRegroupe[i][Config.MCCCVIII];
+         fiche[Config.VIII] += resRegroupe[i][Config.MCCCVIII] + ", ";
       }
       
       for (int cpt = Config.MCCCLIII; cpt < Config.MCDIIX; cpt += Config.II) {
          toRefactor(i, cpt, fiche, resRegroupe);
       }
       if (resRegroupe[i][Config.MCDIX].equals(Config.OUI)) {
-         fiche[Config.IX] += " -" + resRegroupe[i][Config.MCDX];
+         fiche[Config.IX] += resRegroupe[i][Config.MCDX] + ", ";
       }
 
       fiche[Config.XI] = Config.CONTACTEXPERT[i][Config.O];
       fiche[Config.XII] = Config.CONTACTEXPERT[i][Config.I];
       fiche[Config.XIII] = Config.CONTACTEXPERT[i][Config.II];
       fiche[Config.XIV] = Config.CONTACTEXPERT[i][Config.III];
-      fiche[Config.XV] = Config.URL[i];
+      fiche[Config.XV] = Config.CONTACTEXPERT[i][Config.IV];
+      fiche[Config.XVI] = Config.URL[i];
+      fiche[Config.XXI] = Config.ENTITYID[i];
       if (Test.isDifferentToSpace(resRegroupe, i, Config.MCDXI)) {
-          fiche[Config.XVIII] = resRegroupe[i][Config.MCDXI];
-       }
+          fiche[Config.XIX] = resRegroupe[i][Config.MCDXI];
+      }
+      fiche[Config.XX] = Integer.toString(i);
    }
    
    static void toGenerate(String[] fiche) throws IOException {
@@ -181,7 +173,7 @@ public class Algo2 {
             cellule[j][Config.I] = fiche[j];
          }
          ReadWriteExcelFile.writeXlsxFile(xlsxFile, cellule);
-         WriteXMLFile.generer(fiche);
+         WriteXMLFile.generer(fiche, fiche[Config.XXII]);
       }
     }
 }
